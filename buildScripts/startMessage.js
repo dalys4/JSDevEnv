9f984d9869708f107173f05d9807b69264997ccf
Daly, Scott <DALYS4@nationwide.com>
2019-11-21 13:36:07 -0500

    comment out babel

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,4 @@
-//import chalk from 'chalk';
-var chalk = require('chalk');
+//import chalk from 'chalk';  //ES6
+var chalk = require('chalk'); //ES5
 
 console.log(chalk.green('Starting app in dev mode...'));
